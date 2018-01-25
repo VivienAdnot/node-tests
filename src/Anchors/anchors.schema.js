d5d797860a30aedb6bd323f6eecd6f251eea6e61
Vivien Adnot <vivienadnot@amplement.com>
2018-01-25 12:10:46 +0100

    ajv check if id exists in database before validate schema

--- src/Anchors/anchors.schema.js
+++ src/Anchors/anchors.schema.js
@@ -3,6 +3,21 @@ var setupAsync = require('ajv-async');
 var constants = require('./anchors.const');
 var ajvUtils = require('../services/utils/ajv/ajv.const');
 
+function getRdbData(shouldSucceed) {
+    const data = (shouldSucceed)
+        ? [{ id: 123 }, { id: 456 }]
+        : [];
+
+    return Promise.resolve(data);
+}
+
+function checkIdExists(schema, data) {
+    return getRdbData(false)
+        .then((rows) => {
+            return !!rows.length;
+        })
+}
+
 const ANCHOR_SCHEMA = {
     $async: true,
     type: 'object',
@@ -10,13 +25,23 @@ const ANCHOR_SCHEMA = {
         _feed: { type: 'string', pattern: ajvUtils.UUID_PATTERN },
         _target: { type: 'string', pattern: ajvUtils.UUID_PATTERN },
         _user: { type: 'string', pattern: ajvUtils.UUID_PATTERN },
-        id: { type: 'string', pattern: ajvUtils.UUID_PATTERN },
+        id: {
+            type: 'string',
+            pattern: ajvUtils.UUID_PATTERN,
+            idExists: { table: 'anchors'}
+        },
         status: { type: 'integer', enum: constants.SUBSCRIPTION_STATUSES },
     },
     required: ['_feed', '_target', '_user']
 };
 
 var ajv = setupAsync(new Ajv(ajvUtils.AJV_BASIC_SETTINGS));
+ajv.addKeyword('idExists', {
+    async: true,
+    type: 'string',
+    validate: checkIdExists
+});
+
 var validate = ajv.compile(ANCHOR_SCHEMA);
 
 module.exports = {
