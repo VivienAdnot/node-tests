31092906b5559a5f732adedb435cddb1fa9b9cc6
Vivien Adnot <vivienadnot@amplement.com>
2018-02-23 14:33:21 +0100

    test with async waterfall ok

--- src/importContacts/importContacts.handlers.js
+++ src/importContacts/importContacts.handlers.js
@@ -27,6 +27,24 @@ exports.writeFile = (req, res, next) => {
 
 exports.testAsync = (req, res, next) => {
 
-    next();
+    async.waterfall([
+        function (callback) {
+            console.log('First Step --> ');
+            callback(null, '1', '2');
+        },
+        function (arg1, arg2, callback) {
+            console.log('Second Step --> ' + arg1 + ' ' + arg2);
+            callback(null, '3');
+        },
+        function (arg1, callback) {
+            console.log('Third Step --> ' + arg1);
+            callback(null, 'final result');
+        }
+    ], function (err, result) {
+        console.log('Main Callback --> ' + result);
+        res.data = result;
+        next();
+    });
 
+    console.log('Program End');
 }
