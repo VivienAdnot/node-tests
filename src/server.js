8e6fda692877dde07bbf78d52b95cb2b161096a1
Vivien Adnot <vivienadnot@amplement.com>
2018-06-29 16:39:10 +0200

    renamed Raven to raven

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import Raven from 'raven';
+import raven from 'raven';
 import { run } from './bootstrap';
 import { errorResponseSender } from './services/responseSender';
 
@@ -10,24 +10,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const sentryUrl = 'https://b722fedddaa648aaaa4146df487e02d0@sentry.io/1200283';
-Raven
+raven
     .config(sentryUrl, {
         captureUnhandledRejections: true
     })
     .install();
 
-app.use(Raven.requestHandler());
+app.use(raven.requestHandler());
 
 app.get('/', (req, res) => res.send({ serverLive: true }));
 
 run(app);
 
-Raven.setContext({
+raven.setContext({
     tags: {
         errorType: 'uncaught'
     }
 });
-app.use(Raven.errorHandler());
+app.use(raven.errorHandler());
 
 app.use(errorResponseSender);
 
